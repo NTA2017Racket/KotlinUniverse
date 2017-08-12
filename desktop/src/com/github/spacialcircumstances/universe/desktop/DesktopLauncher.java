5e1836b890b3fc3ea3ace637a9afceb843b0bd9c
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-08-12 16:27:34 +0200

    Initial commit

---
+++ desktop/src/com/github/spacialcircumstances/universe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.spacialcircumstances.universe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.spacialcircumstances.universe.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
