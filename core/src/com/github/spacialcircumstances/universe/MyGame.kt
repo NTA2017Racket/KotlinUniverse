b63c830feccefcdcd05c06b1a8fde67b7593dfc5
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-08-13 14:32:28 +0200

    Draw space objects. Generate map.

--- core/src/com/github/spacialcircumstances/universe/MyGame.kt
+++ core/src/com/github/spacialcircumstances/universe/MyGame.kt
@@ -5,17 +5,28 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.utils.viewport.FitViewport
+import java.security.SecureRandom
 
 class MyGame : ApplicationAdapter() {
     lateinit var stage: Stage
 
     lateinit var background: BackgroundActor
+    var objectsList: MutableList<SpaceObject> = mutableListOf()
+
+    val random = SecureRandom()
+
+    lateinit var MoonTexture: Texture
+    lateinit var PlanetTexture: Texture
+    lateinit var SunTexture: Texture
 
     override fun create() {
         stage = Stage()
         background = BackgroundActor(Texture("Background.png"))
         stage.addActor(background)
+        MoonTexture = Texture("Moon.png")
+        PlanetTexture = Texture("Planet.png")
+        SunTexture = Texture("Sun.png")
+        createMap()
     }
 
     override fun render() {
@@ -26,6 +37,30 @@ class MyGame : ApplicationAdapter() {
         stage.draw()
     }
 
+    fun createMap() {
+        for (i in 0..20) {
+            var rad = random.nextInt(40) + 15f
+            var posx = random.nextInt(1600)
+            var posy = random.nextInt(900)
+            var obj = SpaceObject(rad, getObjectTexture(rad))
+            obj.setPosition(posx.toFloat(), posy.toFloat())
+            objectsList.add(obj)
+            stage.addActor(obj)
+        }
+    }
+
+    fun getObjectTexture(radius: Float): Texture {
+        if(radius < 25) {
+            return MoonTexture
+        } else if (radius < 40) {
+            return PlanetTexture
+        } else {
+            return SunTexture
+        }
+
+    }
+
     override fun dispose() {
+        stage.dispose()
     }
 }
---
+++ core/src/com/github/spacialcircumstances/universe/SpaceObject.kt
@@ -0,0 +1,19 @@
+package com.github.spacialcircumstances.universe
+
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Batch
+
+class SpaceObject(val radius: Float, val tex: Texture): Actor() {
+    var drawx = x - tex.width / 2
+    var drawy = y - tex.height / 2
+    override fun draw(batch: Batch?, parentAlpha: Float) {
+        batch!!.draw(tex, drawx, drawy, radius, radius)
+    }
+
+    override fun setPosition(x: Float, y: Float) {
+        super.setPosition(x, y)
+        drawx = x - tex.width / 2
+        drawy = y - tex.height / 2
+    }
+}
