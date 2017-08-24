d412af287c7d673b7c124da6222c43007ce59a6f
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-08-24 11:42:45 +0200

    Rotate projectiles so they behave like missiles

--- core/src/com/github/spacialcircumstances/universe/ProjectileActor.kt
+++ core/src/com/github/spacialcircumstances/universe/ProjectileActor.kt
@@ -3,20 +3,23 @@ package com.github.spacialcircumstances.universe
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.Sprite
 
 class ProjectileActor(var player: PlayerActor, var tex: Texture): CircleCollidingActor(5f) {
     var velocityX = 0f
     var velocityY = 0f
     var accelerationX = 0f
     var accelerationY = 0f
+    val sprite = Sprite(tex, 0, 0, 10, 10)
     init {
         x = player.x
         y = player.y
+        sprite.setPosition(x, y)
+        sprite.texture = tex
+        sprite.color = player.playerColor
     }
     override fun draw(batch: Batch?, parentAlpha: Float) {
-        batch!!.color = player.playerColor
-        batch!!.draw(tex, x - radius , y - radius, radius * 2, radius * 2)
-        batch!!.color = Color.WHITE
+        sprite.draw(batch)
     }
 
     override fun act(delta: Float) {
@@ -24,5 +27,8 @@ class ProjectileActor(var player: PlayerActor, var tex: Texture): CircleCollidin
         velocityY += accelerationY * delta
         x += velocityX * delta
         y += velocityY * delta
+        sprite.setPosition(x, y)
+        rotation = ((Math.atan((velocityY / velocityX).toDouble()) / (2 * Math.PI)).toFloat() * 360) - 90
+        sprite.rotation = rotation
     }
 }
