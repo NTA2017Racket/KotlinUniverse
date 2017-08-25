d69c4d22203aff5433ab6d5552bc5262eb4c43cb
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-08-25 11:26:56 +0200

    Sort player results by kills

--- core/src/com/github/spacialcircumstances/universe/StatsActor.kt
+++ core/src/com/github/spacialcircumstances/universe/StatsActor.kt
@@ -4,10 +4,13 @@ import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.scenes.scene2d.Actor
 
-class StatsActor(var font: BitmapFont): Actor() {
+class StatsActor(var font: BitmapFont) : Actor() {
     var statsDisplay: MutableList<String> = mutableListOf()
     fun initialize(players: Collection<PlayerActor>) {
-        players.forEach({
+        players.sortedWith(compareByDescending({
+            it.killStat
+        })
+        ).forEach({
             statsDisplay.add(it.playerName)
             statsDisplay.add("Kills: " + it.killStat.toString())
             statsDisplay.add("Deaths: " + it.deathStat.toString())
@@ -20,11 +23,11 @@ class StatsActor(var font: BitmapFont): Actor() {
 
     override fun draw(batch: Batch?, parentAlpha: Float) {
         super.draw(batch, parentAlpha)
-        for(i in 0..(statsDisplay.size - 1)) {
+        for (i in 0..(statsDisplay.size - 1)) {
             val p = i % 3
             if (p < 3) {
                 font.draw(batch, statsDisplay[i], 100f, 800f - (i * 40f))
-            }            else {
+            } else {
                 font.draw(batch, statsDisplay[i], 400f, 800f - ((i - 9) * 40f))
             }
         }
