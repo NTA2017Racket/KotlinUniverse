ea3e9392f48eb8fad0c766f53d2bcfb62043b2f6
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-08-25 11:50:34 +0200

    Revert mass change

--- core/src/com/github/spacialcircumstances/universe/SpaceObject.kt
+++ core/src/com/github/spacialcircumstances/universe/SpaceObject.kt
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.g2d.Batch
 class SpaceObject(radius: Float, val tex: Texture): CircleCollidingActor(radius) {
     var drawx = x - radius
     var drawy = y - radius
-    var mass = (radius * radius * radius) * 3.2f
+    var mass = (radius * radius * radius) * 3f
     override fun draw(batch: Batch?, parentAlpha: Float) {
         batch!!.draw(tex, drawx, drawy, 2 * radius, 2 * radius)
     }
